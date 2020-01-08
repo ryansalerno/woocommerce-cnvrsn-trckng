c9ccce5cecc9d460d576d3d0642cbfdd355f32f1
ryansalerno <rsalerno@gmail.com>
2020-01-08 16:01:02 -0700

    Add full license and remove inline comment

--- conversion-tracking.php
+++ conversion-tracking.php
@@ -5,36 +5,13 @@ Plugin URI: https://github.com/ryansalerno/woocommerce-cnvrsn-trckng
 Description: Forked from the excellent WooCommerce Conversion Tracking plugin by Tareq Hasan (with lots of bits removed)
 Version: 0.1.0
 Author: Ryan (after Tareq Hasan)
-License: GPL2
+License: GPL3
 WC requires at least: 2.3
 WC tested up to: 3.8.1
 */
 
-/**
- * Original code Copyright (c) 2017 Tareq Hasan (email: tareq@wedevs.com). All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * **********************************************************************
- */
+// Original code Copyright (c) 2017 Tareq Hasan (email: tareq@wedevs.com). All rights reserved.
+// (License moved to its own file)
 
 // don't call the file directly
 if ( ! defined( 'ABSPATH' ) ) {
